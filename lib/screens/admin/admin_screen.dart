53813671f07e38bf8a3b5de618ced52583d78f66
Sanjeeth18 <sanjeeth653@gmail.com>
2026-09-21 01:56:37 +0530

    removed lifeclycle update

--- lib/screens/admin/admin_screen.dart
+++ lib/screens/admin/admin_screen.dart
@@ -26,30 +26,13 @@ class AdminScreen extends ConsumerStatefulWidget {
   ConsumerState<AdminScreen> createState() => _AdminScreenState();
 }
 
-class _AdminScreenState extends ConsumerState<AdminScreen> with WidgetsBindingObserver {
+class _AdminScreenState extends ConsumerState<AdminScreen> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addObserver(this);
     WidgetsBinding.instance.addPostFrameCallback((_) {
       ref.read(tokenCountsProvider.notifier).refresh();
     });
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      ref.read(tokenCountsProvider.notifier).refresh();
-      ref.invalidate(purchasesHistoryProvider);
-      ref.invalidate(redemptionsHistoryProvider);
-      ref.invalidate(eggTokenSummaryProvider);
-    }
   }
 
   Future<void> _handleLogout() async {
